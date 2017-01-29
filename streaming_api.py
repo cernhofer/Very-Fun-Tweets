1bab4d5c1bc138bcf22e325c086c1940e3a998cc
cernhofer <cernhofer@gmail.com>
2017-01-28 21:35:37 -0600

    creating mongo client

--- streaming_api.py
+++ streaming_api.py
@@ -1,8 +1,10 @@
+from pymongo import MongoClient
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from dotenv import load_dotenv, find_dotenv
 import os
+import pdb
 
 load_dotenv(find_dotenv())
 
@@ -11,6 +13,7 @@ ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
 CONSUMER_KEY_SECRET = os.environ.get("CONSUMER_KEY_SECRET")
+MONGODB_URI = os.environ.get("MONGODB_URI")
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
@@ -25,11 +28,58 @@ class StdOutListener(StreamListener):
 
 if __name__ == '__main__':
 
+    client = MongoClient(MONGODB_URI)
+
+    db = client.get_default_database()
+
+    tweets = db.tweets 
+    
     #This handles Twitter authetification and the connection to Twitter Streaming API
     l = StdOutListener()
     auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     stream = Stream(auth, l)
 
+    '''
+
+    created_at 
+    id
+    text
+    user
+        id
+        screen_name 
+        location 
+        verified 
+        followers_count 
+        time_zone 
+        lang 
+    geo 
+    coordinates 
+    place 
+    retweet 
+    retweeted status? 
+    entities
+        hashtags 
+        urls
+        user_mentions
+        symbols 
+    timestamp_ms 
+    retweet_count
+    favourite_count
+
+
+    '''
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
     stream.filter(track=['#'])
+
+    print("DONE")
+
+
+
+
+
+
+
+
+
+
