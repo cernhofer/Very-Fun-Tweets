d5c9f9341cd18a1e1ea94e3fbe531c0d6a39de65
Haylee Fay Ham <hayleeham@gmail.com>
2017-03-06 19:09:29 -0600

    Next iteration of whole_rcc

--- whole_rcc.py
+++ whole_rcc.py
@@ -1,15 +1,25 @@
-### THIS IS JUST PSEUDO CODE
+from push_to_postgres import push_to_postgres
+from scraping.news_scraper import * # find out name of function
 
 hashtags = db.tweets.all()
 for hashtag in hashtags:
     if len(hashtag.tweets) < 1000: continue
 
-    has_spike = sush_script(hashtag) # returns bool
+    has_spike, spike_data = sush_script(hashtag) # returns bool
 
     if has_spike:
-        data = prepare_data(hashtag) # outputs a formatted data object
-        data['stories'] = chelsea_script(hashtag)
+        # data = prepare_data(hashtag) # outputs a formatted data object
+        spike_data['stories'] = chelsea_script(hashtag) # or pass the spike data
         push_to_postgres(data)
-    elif is_in_list():
+    elif is_in_list(hashtag):
         # have this push to postgres if hashtag is on list of previously pushed hashtags
-        pass
+        # no need to call chelsea_script, need to handle for no stories bc postgres?
+        push_to_postgres(data)
+
+
+def is_in_list(hashtag):
+    # create a list to be stored in mongo and check if hashtag is in there
+    # returns bool
+
+def add_to_list(hashtag):
+    # write this
