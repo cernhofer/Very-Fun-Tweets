02452731a5b089b993c8e3a931565e6b58f9ece2
Haylee Fay Ham <hayleeham@gmail.com>
2017-03-07 13:21:32 -0600

    spaces to tabs

--- django_app/tweets/tweet_search/views.py
+++ django_app/tweets/tweet_search/views.py
@@ -30,29 +30,29 @@ def search(request):
 
 
 def populate(request):
-    if request.method == 'GET':
-        csrf.get_token(request)
-        return HttpResponse('ok')
+	if request.method == 'GET':
+		csrf.get_token(request)
+		return HttpResponse('ok')
 
-    payload = json.loads(request.POST.get('payload'))
+	payload = json.loads(request.POST.get('payload'))
 
-    # Delete any old data that exists
+	# Delete any old data that exists
 	hashtag_in_db = Hashtag.objects.filter(name=payload.get('hashtag'))
 	hashtag_in_db.all().delete()
 
-    hashtag = Hashtag(name=payload.get('hashtag'))
-    hashtag.save()
+	hashtag = Hashtag(name=payload.get('hashtag'))
+	hashtag.save()
 
-    for tweetbucket in payload.get('tweetbuckets'):
-        timestamp = tweetbucket.get('timestamp')
-        count = tweetbucket.get('count')
-        tb = TweetBucket(timestamp=timestamp, count=count, hashtag=hashtag)
-        tb.save()
+	for tweetbucket in payload.get('tweetbuckets'):
+		timestamp = tweetbucket.get('timestamp')
+		count = tweetbucket.get('count')
+		tb = TweetBucket(timestamp=timestamp, count=count, hashtag=hashtag)
+		tb.save()
 
-    for story in payload.get('stories'):
-        timestamp = story.get('timestamp')
-        url = story.get('url')
-        s = Story(timestamp=timestamp, url=url, hashtag=hashtag)
-        s.save()
+	for story in payload.get('stories'):
+		timestamp = story.get('timestamp')
+		url = story.get('url')
+		s = Story(timestamp=timestamp, url=url, hashtag=hashtag)
+		s.save()
 
-    return HttpResponse('ok')
+	return HttpResponse('ok')
