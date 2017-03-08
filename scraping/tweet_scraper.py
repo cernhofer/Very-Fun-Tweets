493056b6b9a27e89dd59206b7748c0229dfd88e7
cernhofer <cernhofer@gmail.com>
2017-03-07 18:58:16 -0600

    testing

--- scraping/tweet_scraper.py
+++ scraping/tweet_scraper.py
@@ -34,7 +34,7 @@ def scrape_tweet(word_string, word_list):
 			word = word.lower()
 			word = ''.join(x for x in word if x in string.printable)
 			if word not in INDEX_IGNORE:
-				if not re.match(r'^#', word) and not re.match(r'^@', word) and not re.match(r'^https', word) and not re.match(r'^http', word) and word is not "":
+				if not re.match(r'^#', word) and not re.match(r'^@', word) and not re.match(r'^https', word) and not re.match(r'^http', word) and word is not "" and word != "":
 					word_list.append(re.sub('[^a-zA-Z]+', '', word))
 
 def run_for_your_life(hashtag_list):
