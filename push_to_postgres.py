ded62800a1475d7bf1df3bdecb7c9d7ede108bf1
Haylee Fay Ham <hayleeham@gmail.com>
2017-03-13 17:04:31 -0500

    documenting and cleaning

--- push_to_postgres.py
+++ push_to_postgres.py
@@ -2,20 +2,24 @@ import json
 import requests
 
 def push_to_postgres(data):
+    '''
+    Create a session with Django app and post payload to the client
+    '''
     REQUEST_BASE = 'http://www.veryfuntweets.com'
     REQUEST_PATH = 'tweet_search/populate'
     REQUEST_URL = '/'.join([REQUEST_BASE, REQUEST_PATH])
 
     client = requests.session()
 
-    # retrieve the CSRF token first
+    # retrieve the CSRF token
     client.get(REQUEST_URL)
-
     csrftoken = client.cookies['csrftoken']
 
+    # create payload dictionary
     payload = dict(
     csrfmiddlewaretoken=csrftoken,
     payload=json.dumps(data)
     )
 
+    # post payload to client
     response = client.post(REQUEST_URL, data=payload)
--- whole_rcc.py
+++ whole_rcc.py
@@ -6,6 +6,7 @@ import pymongo
 from dotenv import load_dotenv, find_dotenv
 import os, json
 
+# variables that contain user credentials to access MongoDB
 load_dotenv(find_dotenv())
 MONGODB_URI = os.environ.get("MONGODB_URI")
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
@@ -13,39 +14,46 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME")
 client = pymongo.MongoClient(MONGODB_URI)
 db = client[DATABASE_NAME]
 
-total_count = 16992
+# setting current total count and also hashtag count that resets for each
+# iteration through the for loop below, in order to keep track of which data
+# from the mongoDB has been processed
+total_count = 0
 hashtag_count = 0
 
-while True:
+# total number of hashtags in the MongoDB
+num_hashtags = db.tweets.count()
+
+# this while loop and try except is there for when the pymongo cursor times out
+while total_count < num_hashtags:
     try:
+        # skip to where we are in the database
         hashtags = db.tweets.find({}).skip(total_count)
 
+        # iterate through each hashtag object and detect spikes and news stories
+        # for any hashtags with greater than 1000 tweets
         for hashtag in hashtags:
             hashtag_count += 1
-            print("\n\n\nThis is total count", total_count)
-            print("This is hashtag number", hashtag_count)
 
             if len(hashtag['tweets']) < 1000: continue
 
             has_spike, date_list, spike_data, tweet_text_list = spikes(hashtag)
 
             if has_spike:
-                print("SPIKE!!")
                 hashtag_name = spike_data['hashtag']
                 print(hashtag_name)
 
                 common_words = run_for_your_life(tweet_text_list, hashtag_name)
                 if common_words is not None:
-                    print('Calling run_baby_run')
                     news_story = run_baby_run(hashtag_name, date_list, common_words)
 
                     if news_story is not None:
                         spike_data['stories'] = news_story
 
-                        print('Pushing to postgres')
+                        # push hashtag data to postgres if spike and news stories
+                        # are both present
                         push_to_postgres(spike_data)
-                else:
-                    print("hashtag isn't news worthy :/")
+
+    # if cursor times out, update total_count, hashtag_count and restart
     except pymongo.errors.CursorNotFound:
         total_count = total_count + hashtag_count
         hashtag_count = 0
