2ea969b53a85fe3354dbb95be7b414d63c63f2af
Haylee Fay Ham <hayleeham@gmail.com>
2017-03-13 17:23:36 -0500

    setting debug to false

--- django_app/tweets/tweets/settings.py
+++ django_app/tweets/tweets/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'murmuring-savannah-25002.herokuapp.com',
